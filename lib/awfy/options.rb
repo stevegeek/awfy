c9ed998c803e5f4b6de837451e8f882165025101
Stephen Ierodiaconou <stevegeek@gmail.com>
2024-10-31 06:34:11 +0100

    Options can just be a Data

--- lib/awfy/options.rb
+++ lib/awfy/options.rb
@@ -1,31 +1,17 @@
 # frozen_string_literal: true
 
 module Awfy
-  class Options
-    def initialize(verbose:, summary:, summary_format:, temp_output_directory:, setup_file_path:, tests_path:, compare_with_branch:, assert:, runtime:)
-      @verbose = verbose
-      @summary = summary
-      @summary_format = summary_format
-      @temp_output_directory = temp_output_directory
-      @setup_file_path = setup_file_path
-      @tests_path = tests_path
-      @compare_with_branch = compare_with_branch
-      @assert = assert
-      @runtime = runtime
-    end
-
+  Options = Data.define(:verbose, :summary, :summary_format, :temp_output_directory, :setup_file_path, :tests_path, :compare_with_branch, :assert, :runtime) do
     def yjit_only? = runtime == "yjit"
 
     def both_runtimes? = runtime == "both"
 
-    def show_summary? = @summary
+    def show_summary? = summary
 
-    def verbose? = @verbose
+    def verbose? = verbose
 
-    def assert? = @assert
+    def assert? = assert
 
-    def humanized_runtime = @runtime.upcase
-
-    attr_reader :temp_output_directory, :setup_file_path, :tests_path, :compare_with_branch, :runtime, :summary_format
+    def humanized_runtime = runtime.upcase
   end
 end
